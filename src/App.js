62cbe3d350ee64e36579db7170fa9f03a119d466
alonchete <alonsete.tyr@gmail.com>
2022-12-14 12:37:19 +0100

    flex react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
 
+import logo from './assets/drink.svg';
+import barra from './img/barra.jpg';
+import './css/main.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <>
+    <header class="nav">
+
+    <div class="nav__container">
+    <h1 class="nav__logo">
+        <img src={logo}/>
+
+    </h1>
+
+    <label for="menu" class="nav__label">
+
+        <img src="assets/menu.svg" alt="nav__img"/>
+
+    </label>
+
+    <input type="checkbox" id="menu" class="nav__input"/>
+    
+    <div class="nav__menu">
+        <a href="#" class="nav__item">Home</a>
+        <a href="#" class="nav__item">Categorías</a>
+        <a href="#" class="nav__item">Algo</a>
+        <a href="#" class="nav__item">contacto</a>
     </div>
-  );
+
+</div>
+</header>
+
+<div class="cabecera">
+<img src={barra}/>
+<div class="COCOKTAIL">COCOKTAIL.</div>
+</div>
+
+<div class="categorias">
+
+<div class="card">
+
+    <div class="card-bg">
+
+        <img src="img/img16.jpg" alt="..."/>
+
+    </div>
+
+    <div class="card-context">
+
+        <div class="dark-bg"></div>
+
+        <h2>Categoria1</h2>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Sapiente tenetur dicta cupiditate.
+        </p>
+
+        <button>Ver más</button>
+
+    </div>
+
+</div>
+<div class="card">
+
+    <div class="card-bg">
+
+        <img src="img/img16.jpg" alt="..."/>
+
+    </div>
+
+    <div class="card-context">
+
+        <div class="dark-bg"></div>
+
+        <h2>Categoria2</h2>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Sapiente tenetur dicta cupiditate.
+        </p>
+
+        <button>Ver más</button>
+
+    </div>
+    
+</div>
+<div class="card">
+
+    <div class="card-bg">
+
+        <img src="img/img16.jpg" alt="..."/>
+
+    </div>
+
+    <div class="card-context">
+
+        <div class="dark-bg"></div>
+
+        <h2>Categoria3</h2>
+
+        <p>Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Sapiente tenetur dicta cupiditate.
+        </p>
+
+        <button>Ver más</button>
+
+    </div>
+    
+</div>
+</div>
+
+
+    </>
+    
+  ) 
+
+
+  
+  
+ 
 }
 
 export default App;
